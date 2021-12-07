492fe8a843f30f9c5e53cfe8953696c230237486
KhjMeister <95680946+KhjMeister@users.noreply.github.com>
2021-12-07 15:26:23 +0330

    Update index.php

--- Php/index.php
+++ Php/index.php
@@ -1,33 +1,4 @@
 <?php
- 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "word1";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// $sql = "SELECT * FROM sebri";
-// $result = $conn->query($sql);
-
-// if ($result->num_rows > 0) {
- 
-//   while($row = $result->fetch_assoc()) {
-//     echo  $row["id"]. "  " . $row["qst"]."<br>";
-//   }
-// } else {
-//   echo "0 results";
-// }
-// $conn->close();
-
-
-
-
 $striped_content = '';
         $content = '';
 
